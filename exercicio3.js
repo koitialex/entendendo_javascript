8c8547438ff8481ef5aa18c13c2e3478a100f38a
koitialex <koitialex@gmail.com>
2024-08-22 21:52:48 -0300

    feat: execício 3

---
+++ exercicio3.js
@@ -0,0 +1,10 @@
+let n1, n2 = 0;
+n1 = parseFloat(prompt ("digite o primeiro número"));
+n2 = parseFloat(prompt ("digite o segundo número"));
+if (n1 > n2){
+    mensagem = " O maior número é o primeiro digitado "
+}
+else 
+    mensagem = "O maior número é o segundo digitado "
+
+alert(mensagem);
