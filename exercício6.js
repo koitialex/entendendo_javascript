337bd08adc39e287331ad6d8a2fd154c3b1a7b29
koitialex <koitialex@gmail.com>
2024-08-23 21:54:27 -0300

    feat: exercício 6

---
+++ exercício6.js
@@ -0,0 +1,10 @@
+let nome, salario, renda = " "
+let horas = 0
+nome = prompt (" Qual é o seu nome ?");
+salario = parseFloat(prompt (" Quando ganha por hora ?"));
+horas = parseFloat(prompt (" Quantas horas trabalhadas por semana ?"));
+
+renda = salario * horas;
+
+mensagem = nome + " ganha " + renda; 
+alert (mensagem)
